99d59b62c8ced521c749b9b22d4d7353aaf9ffa8
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-05-25 21:56:58 -0500

    Python Solved

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,17 @@
 def fibonacci(n):
-  pass
+  holder = [1]
+  next_number = 1
+  fib = [1, 1]
 
+  if n == 0:
+    return 0
+  if n <= 2:
+    return 1
 
+  while len(fib) < n:
+    holder.append(next_number)
+    next_number = next_number + holder[0]
+    fib.append(next_number)
+    holder.pop(0)
+  
+  return fib.pop()
